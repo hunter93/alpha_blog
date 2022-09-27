ca6c45c61179ee74653109cd1aa52b0d710793d5
Hunter Ogden <87664979+hunter93@users.noreply.github.com>
2022-09-27 18:40:29 -0400

    DRYing up code in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -22,22 +22,25 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(params.require(:article).permit(:title, :description))
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
       render 'edit'
     end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
