0cd96a7ec35e03344013b2cfdd2f085d7f087880
Hunter Ogden <hunter.ogden@workwave.com>
2022-12-04 23:27:49 -0500

    adding back in js for hover dropdown and adding fix for uglifier

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,11 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+$(document).ready(function(){
+  $('.navbar .dropdown').hover(function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+  }, function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
+  });
+})
