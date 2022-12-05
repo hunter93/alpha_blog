da7260d9a0001f1556412c640b0c5e8e41784052
Hunter Ogden <hunter.ogden@workwave.com>
2022-12-04 23:17:24 -0500

    removing js custom code for hover from application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,11 +17,3 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-
-$(document).ready(function(){
-  $('.navbar .dropdown').hover(function() {
-      $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
-  }, function() {
-      $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
-  });
-})
