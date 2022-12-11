a5171cb22a3f14e80fd82271e183bb7cbfc3602e
Hunter Ogden <hunter.ogden@workwave.com>
2022-12-10 20:01:21 -0500

    validations for user: email and username; uniqueness and length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+    uniqueness: { case_sensitive: false },
+    length: {minimum: 3, maximimum: 25}
+  validates :email, presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { minimum: 1, maximimum: 105 }
 end
