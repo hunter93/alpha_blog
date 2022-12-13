26fdc23592e974c18e41a15454b7d1c20731617b
Hunter Ogden <hunter.ogden@workwave.com>
2022-12-13 01:11:36 -0500

    add has_secure_password, install bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :email, presence: true,
     uniqueness: { case_sensitive: false },
     length: { minimum: 1, maximimum: 105 }
+    has_secure_password
 end
